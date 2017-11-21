3f4ab1a729806ec6e6fa50d8fa8513da46666b1b
OstapOcean <Ostap@Mac-Ostap.local>
2017-11-21 18:03:25 +0200

    Third version

--- calculator-3.hs
+++ calculator-3.hs
@@ -15,13 +15,13 @@ import Data.Tuple
 
 add :: (Float, (Float, [[Char]])) -> (Float, [[Char]])
 sub :: (Float, (Float, [[Char]])) -> (Float, [[Char]])
-mul :: (Float, Float) -> Float
-prt :: (Float, Float) -> Float
+mul :: (Float, (Float, [[Char]])) -> (Float, [[Char]])
+prt :: (Float, (Float, [[Char]])) -> (Float, [[Char]])
 
 add (a, (b, expr)) = (a + b, expr)
 sub (a, (b, expr)) = (a - b, expr)
-mul (a, b) = a * b
-prt (a, b) = a / b
+mul (a, (b, expr)) = (a * b, expr)
+prt (a, (b, expr)) = (a / b, expr)
 
 toFloat :: [Char] -> Float
 toFloat x = read x :: Float
@@ -38,10 +38,9 @@ mInput (expr) = fInput(toFloat(head expr), tail expr)
  
 fInput :: (Float, [[Char]]) -> (Float, [[Char]])
 fInput (fAcc, expr) | null expr                  = (fAcc, [])
-                    | null cutExpr               = (fAcc, [])
-                    | expr == []                 = (fAcc, [])
-                    | nOpr == "+" || nOpr == "-" = fCalc(fAcc, cOpr, (nNum, expr))
-                    | nOpr == "*" || nOpr == "/" = fCalc(fAcc, cOpr, sInput(nNum, cutExpr))
+                    | null cutExpr               = fCalcF(fAcc, cOpr, (nNum, cutExpr))
+                    | nOpr == "+" || nOpr == "-" = fCalc (fAcc, cOpr, (nNum, cutExpr))
+                    | nOpr == "*" || nOpr == "/" = fCalc (fAcc, cOpr, sInput(nNum, cutExpr))
                     | otherwise                  = (fAcc, [])
                     where
                     cutExpr = tail(tail expr)
@@ -50,41 +49,37 @@ fInput (fAcc, expr) | null expr                  = (fAcc, [])
                     nOpr    =         head(tail(tail expr))
 
 fCalc :: (Float, [Char], (Float, [[Char]])) -> (Float, [[Char]])
-fCalc (fAcc, cOpr, (nNum, expr)) | null expr    = (fAcc, [])
-                                 | null cutExpr = fCalcF(fAcc, cOpr, (nNum, expr))
-                                 | expr == []   = fCalcF(fAcc, cOpr, (nNum, expr))
-                                 | cOpr == "+"  = fInput(add(fAcc, (nNum, cutExpr)))
-                                 | cOpr == "-"  = fInput(sub(fAcc, (nNum, cutExpr)))
-                                 | cOpr == "*"  = fInput(mul(fAcc, nNum), cutExpr)
-                                 | cOpr == "/"  = fInput(prt(fAcc, nNum), cutExpr)
+fCalc (fAcc, cOpr, (nNum, expr)) | null expr    = fCalcF(fAcc, cOpr, (nNum, expr))
+                                 | cOpr == "+"  = fInput(add(fAcc, (nNum, expr)))
+                                 | cOpr == "-"  = fInput(sub(fAcc, (nNum, expr)))
+                                 | cOpr == "*"  = fInput(mul(fAcc, (nNum, expr)))
+                                 | cOpr == "/"  = fInput(prt(fAcc, (nNum, expr)))
                                  | otherwise    = (fAcc, [])
-                                 where
-                                 cutExpr = tail(tail expr)
                   
 fCalcF :: (Float, [Char], (Float, [[Char]])) -> (Float, [[Char]])
-fCalcF (fAcc, cOpr, (nNum, expr)) | cOpr == "+" = add(fAcc, (nNum, expr))
-                                  | cOpr == "-" = sub(fAcc, (nNum, expr))
-                                  -- | cOpr == "*" = fInput(mul(fAcc, nNum), tail(tail expr))
-                                  -- | cOpr == "/" = fInput(prt(fAcc, nNum), tail(tail expr))
+fCalcF (fAcc, cOpr, (nNum, expr)) | cOpr == "+" = add(fAcc, (nNum, []))
+                                  | cOpr == "-" = sub(fAcc, (nNum, []))
+                                  | cOpr == "*" = mul(fAcc, (nNum, []))
+                                  | cOpr == "/" = prt(fAcc, (nNum, []))
                                   | otherwise   = (fAcc, [])
    
  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 sInput :: (Float, [[Char]]) -> (Float, [[Char]])
-sInput (sAcc, expr) | null expr = (sAcc, expr)
-                    | null cutExpr = sCalcF(sAcc, expr)
-                    -- | nOpr == "+" || nOpr == "-" = sCalcF(sAcc, expr)
+sInput (sAcc, expr) | null expr                  = (sAcc, expr)
+                    | null cutExpr               = sCalcF(sAcc, expr)
+                    | nOpr == "+" || nOpr == "-" = sCalcF(sAcc, expr)
                     | nOpr == "*" || nOpr == "/" = sCalc (sAcc, expr)
-                    | otherwise = (sAcc, [])
+                    | otherwise                  = (sAcc, [])
                     where
                     cutExpr = tail(tail expr)
                     nOpr    = head(cutExpr)
              
 sCalc :: (Float, [[Char]]) -> (Float, [[Char]])
-sCalc (sAcc, expr) | null expr = (sAcc, [])
-                   | cOpr == "*" = sInput(mul(sAcc, nNum), cutExpr)
-                   | cOpr == "/" = sInput(prt(sAcc, nNum), cutExpr)
-                   | otherwise = (sAcc, [])
+sCalc (sAcc, expr) | null expr   = (sAcc, [])
+                   | cOpr == "*" = sInput(mul(sAcc, (nNum, cutExpr)))
+                   | cOpr == "/" = sInput(prt(sAcc, (nNum, cutExpr)))
+                   | otherwise   = (sAcc, [])
                    where
                    cutExpr = tail(tail expr)
                    cOpr    = head expr
@@ -92,10 +87,10 @@ sCalc (sAcc, expr) | null expr = (sAcc, [])
                    nNum    = toFloat(head(tail expr))
                   
 sCalcF :: (Float, [[Char]]) -> (Float, [[Char]])
-sCalcF (sAcc, expr) | null expr = (sAcc, [])
-                    | cOpr == "*" = (mul(sAcc, nNum), cutExpr)
-                    | cOpr == "/" = (prt(sAcc, nNum), cutExpr)
-                    | otherwise = (sAcc, [])
+sCalcF (sAcc, expr) | null expr   = (sAcc, [])
+                    | cOpr == "*" = mul(sAcc, (nNum, cutExpr))
+                    | cOpr == "/" = prt(sAcc, (nNum, cutExpr))
+                    | otherwise   = (sAcc, [])
                     where
                     cutExpr = tail(tail expr)
                     cOpr    = head expr
